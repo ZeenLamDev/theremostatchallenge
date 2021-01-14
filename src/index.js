db066b77e678975fe68f2b9a559adaef12a97d73
ZeenLamDev <Zeen.Lam93@gmail.com>
2021-01-14 17:32:58 +0000

    added jQuery cdn and created a index.js file for jQuery. up button changes temperature, have method in jQuery and vanillaJS

---
+++ src/index.js
@@ -0,0 +1,20 @@
+var thermostat = new Thermostat();
+
+$('#temperature-up').on('click', function() { // event listener
+  thermostat.up(); // update model
+  $('#temperature').text(thermostat.temperature); // update view
+})
+
+
+
+
+
+// Vanilla Js
+
+  // var thermostat = new Thermostat
+  
+  // document.getElementById("temperature-up").addEventListener('click', function() {
+  //   console.log(thermostat)
+  //   thermostat.up()
+  //   document.getElementById("temperature").innerHTML = thermostat.temperature
+  // })
